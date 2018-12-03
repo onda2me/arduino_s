5e0c9e3074376e504a5a940d06004f7fbc32adfd
onda2me <onda2me@users.noreply.github.com>
2018-12-03 21:01:41 +0900

    first

---
+++ s1/sketch_sensor_dht/sketch_sensor_dht.ino
@@ -0,0 +1,33 @@
+
+#include <DHT.h>
+
+#define DHTTYPE DHT11
+#define DHTPIN 2
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  // put your setup code here, to run once:
+
+  dht.begin();
+  Serial.begin(9600);
+}
+
+float h = 0;
+float t = 0;
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  h = dht.readHumidity();     // 습도
+  t = dht.readTemperature();  // 온도
+
+  Serial.print("\n\nh ::: ");
+  Serial.println(h);
+  Serial.print("t ::: ");
+  Serial.println(t);
+
+
+  delay(1000);
+
+}
