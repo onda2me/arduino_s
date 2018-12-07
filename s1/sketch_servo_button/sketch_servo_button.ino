c466a50178d210a20daa5c8f8e7ea10668bd9626
onda2me <onda2me@users.noreply.github.com>
2018-12-07 15:24:38 +0900

    servo 회전각 수정

--- s1/sketch_servo_button/sketch_servo_button.ino
+++ s1/sketch_servo_button/sketch_servo_button.ino
@@ -29,19 +29,27 @@ void loop() {
 
     servo.attach(pinServo);
 
-    // 서보모터 회전시키기 ( 90 -> 180 -> 90 -> 0)
-    servo.write(90);    
+    //시작각
+    Serial.print("서보회전각1 : ");
+    Serial.println(servo.read());  
+    
+    // 서보모터 회전시키기 ( 90 -> 180 -> 0)
+    servo.write(90);
+    Serial.print("서보회전각2 : ");
+    Serial.println(servo.read());   
     delay(1000);
     
-    servo.write(180);   
+    servo.write(180);
+    Serial.print("서보회전각3 : ");
+    Serial.println(servo.read());   
     delay(1000);
-    
-    servo.write(90);   
-    delay(1000);   
-    
-    servo.write(0);    
 
+    //0으로 회전각
+    servo.write(0);
+    Serial.print("서보회전각4 : ");
+    Serial.println(servo.read());  
+    delay(1000);
+        
     servo.detach();
   }
-
 }
